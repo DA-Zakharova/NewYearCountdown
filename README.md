fe5072bcb5d91e77da6d154ad2089673bae6d625
DA-Zakharova <dashazakharova0301@gmail.com>
2025-07-15 15:35:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+🎇 New Year Countdown
+
+A simple yet festive countdown timer to New Year’s Day – January 1, 2026.
+It dynamically displays the remaining days, hours, minutes, and seconds.
+Includes background music that can be played or paused with a button click.
+
+🔧 Built with:
+HTML, CSS, JavaScript.
+
+🛡️ Disclaimer:
+This project is for educational purposes only. All rights to media and music belong to their original creators.
---
+++ script.js
@@ -0,0 +1,54 @@
+function newYearCountdown() {
+    const nyDate = new Date("January 1, 2026 00:00");
+    const now = new Date();
+    const diff = nyDate - now;
+
+    const msInSecond = 1000;
+    const msInMinute = 60 * 1000;
+    const msInHour = 60 * 60 * 1000;
+    const msInDay = 24 * 60 * 60 * 1000;
+
+    const displayDay = Math.floor(diff / msInDay);
+    document.querySelector(".days").textContent = displayDay;
+
+    const displayHour = Math.floor((diff % msInDay) / msInHour);
+    document.querySelector(".hours").textContent = displayHour;
+
+    const displayMinute = Math.floor((diff % msInHour) / msInMinute);
+    document.querySelector(".minutes").textContent = displayMinute;
+
+    const displaySecond = Math.floor((diff % msInMinute) / msInSecond);
+    document.querySelector(".seconds").textContent = displaySecond;
+
+    if (diff <= 0) {
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timerID);
+        happyNY();
+    }
+
+}
+
+let timerID = setInterval(newYearCountdown, 1000);
+
+function happyNY() {
+    const heading = document.querySelector("h1");
+    heading.textContent = "Happy New Year!!!";
+    heading.classList.add("pink");
+}
+
+const btn = document.querySelector("#myButton");
+const audio = document.querySelector("#myAudio");
+const icon = document.querySelector("#icon");
+
+btn.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+        icon.src = "pause.png";
+    } else {
+        audio.pause();
+        icon.src = "play.png";
+    }
+})
